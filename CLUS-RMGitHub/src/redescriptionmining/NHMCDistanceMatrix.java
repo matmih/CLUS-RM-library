c7e05cdae05fa4467194d9336084e1a607b0f0ee
Matej Mihelcic <matmih1@gmail.com>
2018-01-04 18:32:10 +0100

    CLUS-RM java code
    This is the java code of the CLUS-RM algorithm with various types of redescription set optimization and implementing constraint-based redescription mining.

---
+++ CLUS-RMGitHub/src/redescriptionmining/NHMCDistanceMatrix.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package redescriptionmining;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author matej
+ */
+public class NHMCDistanceMatrix {
+
+    NHMCDistanceMatrix(int numElements, ApplicationSettings appset){
+            //not available
+        
+    }
+    
+    void computeDistanceMatrix(RuleReader rr, Mappings map, int MaxDistance, int numElements){
+      //not available
+    }
+    
+    void computeDistanceMatrix(ArrayList<Redescription> redescriptions, Mappings map, int MaxDistance, int numElements, int[] oldRindex){
+       //not available
+    }
+    
+    void loadDistance(File input, Mappings map){
+        
+       //not available
+    }
+    
+    void reset(ApplicationSettings appset){
+        
+       //not available
+    }
+    
+    void writeToFile(File output, Mappings map, ApplicationSettings appset){
+  
+        //not available
+    }
+    
+    void resetFile(File output){
+        //not available
+    }
+    
+}
+
